07999bea7b7dd6ede577ad0ecdd18f1f74e7eab8
Bill Hegazy <bill.hegazy@gmail.com>
2022-04-19 07:48:39 +0700

    add examples

---
+++ examples/image_repository_source_public/main.tf
@@ -0,0 +1,33 @@
+provider "aws" {
+  region = local.region
+}
+
+locals {
+  name   = "hello-app-runner-public"
+  region = "us-east-1"
+
+  tags = {
+    Name = local.name
+  }
+}
+
+module "hello_app_runner" {
+  source = "../../"
+  create = true
+
+  service_name             = local.name
+  tags                     = local.tags
+  auto_deployments_enabled = false # Must set to false to disable auto deployment for EC_PUBLIC type
+  image_repository = [{
+    image_identifier      = "public.ecr.aws/aws-containers/hello-app-runner:latest"
+    image_repository_type = "ECR_PUBLIC"
+    image_configuration = [{
+      port          = 8000
+      start_command = ""
+      runtime_environment_variables = {
+        ENV_VAR_1 = "value1"
+        ENV_VAR_2 = "value2"
+      }
+    }]
+  }]
+}
---
+++ examples/image_repository_source_public/outputs.tf
@@ -0,0 +1,14 @@
+output "service_arn" {
+  description = "The App Runner Service ARN"
+  value       = module.hello_app_runner.service_arn
+}
+
+output "service_url" {
+  description = "The App Runner Service URL"
+  value       = module.hello_app_runner.service_url
+}
+
+output "service_status" {
+  description = "The App Runner Service Status"
+  value       = module.hello_app_runner.service_status
+}
